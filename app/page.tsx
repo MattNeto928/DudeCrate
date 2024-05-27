3486c6732daacc234802d3dc24f683b75e1c9fcb
MattNeto928 <116413270+MattNeto928@users.noreply.github.com>
2024-05-27 16:29:03 -0400

    First demo

---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { LandingPage } from '@/components/LandingPage';
+import shaving from '@/app/shaving.jpg'
+
+function App() {
+  return (
+    <div>
+      <LandingPage />
+    </div>
+  );
+}
+
+export default App;
