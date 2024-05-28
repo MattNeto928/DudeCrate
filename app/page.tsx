f13aa76db42f69e561325d8e15e53c6997e8384b
MattNeto928 <116413270+MattNeto928@users.noreply.github.com>
2024-05-27 23:15:34 -0400

    Added LP carousel

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,11 @@
 import React from 'react';
 import { LandingPage } from '@/components/LandingPage';
-import shaving from '@/app/shaving.jpg'
 import { BottleOpener } from '@/components/BottleOpener';
-import { PinContainer } from '@/components/ui/3d-pin';
 
 function App() {
   return (
     <div>
       <BottleOpener />
-      
     </div>
   );
 }
