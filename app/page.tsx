a16fe358c9e750535d339162084d16b18d550b06
MattNeto928 <116413270+MattNeto928@users.noreply.github.com>
2024-05-27 21:30:46 -0400

    Bottle opener redesign

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import { LandingPage } from '@/components/LandingPage';
 import shaving from '@/app/shaving.jpg'
+import { BottleOpener } from '@/components/BottleOpener';
 
 function App() {
   return (
     <div>
-      <LandingPage />
+      <BottleOpener />
     </div>
   );
 }
